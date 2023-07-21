f8861d1e1a9996ce928e3f51ead078f7d7e319a7
akashbalajikale <105189673+akashbalajikale@users.noreply.github.com>
2023-07-21 10:17:03 +0530

    work on about pages

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+const cl =console.log;
+
+$('.tmslider').owlCarousel({
+    loop :true,
+    nav : true,
+    navText :['<i class="fa-solid fa-arrow-left"></i>','<i class="fa-solid fa-arrow-right"></i>'],
+    responsive : {
+        0 : {
+            items : 1,
+            dots : false
+        },
+        600 : {
+            items : 3
+        },
+        1100 : {
+            items :4
+        }
+    }
+})
